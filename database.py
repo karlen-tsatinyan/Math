31936625e132c1a4c2c69b3647c9fa12c76e4946
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-07-23 17:32:58 -0700

    Update database.py

--- database.py
+++ database.py
@@ -6,11 +6,7 @@ import streamlit as st
 def get_connection():
 
     return psycopg2.connect(
-        host=st.secrets["postgres"]["host"],
-        port=st.secrets["postgres"]["port"],
-        database=st.secrets["postgres"]["database"],
-        user=st.secrets["postgres"]["user"],
-        password=st.secrets["postgres"]["password"],
+        st.secrets["postgres"]["url"]
     )
 
 

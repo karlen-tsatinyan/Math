451e60c93c2cc7bf74fa65ac5f121934f38a0175
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-07-29 19:18:39 -0700

    Add warning for selected homework ID

--- modules/homework.py
+++ modules/homework.py
@@ -28,6 +28,9 @@ def get_homework_file_url(storage_path):
         path = str(storage_path).strip()
 
         st.warning(f"🔎 Original Assignment path being requested: `{path}`")
+        st.warning(
+            f"🔎 Homework ID: `{selected_id}`"
+        )
 
         if not path:
             return None

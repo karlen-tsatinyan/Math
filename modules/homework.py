28c18e6047e34ae8175b785eeb92563a01e34148
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-07-29 19:08:20 -0700

    Update homework.py

--- modules/homework.py
+++ modules/homework.py
@@ -340,8 +340,7 @@ def homework_management():
         elif pd.notna(assignment_file) and str(assignment_file).strip():
         
             storage_path = str(assignment_file).strip()
-            st.warning(f"DEBUG PATH: {storage_path}")
-        
+            st.warning(f"Original assignment path: `{storage_path}`")        
             assignment_url = get_homework_file_url(
                 storage_path
             )

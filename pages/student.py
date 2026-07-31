7fca93c975bb3607ae9562a77e9eb8483b9a2eb9
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-07-30 22:50:41 -0700

    Update welcome message formatting in student dashboard

--- pages/student.py
+++ pages/student.py
@@ -367,9 +367,9 @@ def student_page():
 
 
         st.success(
-            f"Welcome {student['first_name']} \t  |   \t"
-            f"{student['last_name']} \t  |   \t"
-            f"Grade: {student['grade']} \t  |   \t"
+            f"Welcome {student['first_name']} &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; "
+            f"{student['last_name']} &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; "
+            f"Grade: {student['grade']} &nbsp;&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp; "
             f"Subject: {student['subject']}"
         )
 

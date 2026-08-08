3ebfbd0e9cb8bbe4fd7d1705ef4b280dae34a50e
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-08-08 13:21:16 -0700

    Refactor import statement for homework module

--- pages/admin.py
+++ pages/admin.py
@@ -13,10 +13,7 @@ from modules.reports import reports_management
 from modules.scheduler import scheduler_management
 from modules.student_profile import student_profile
 from modules.students import student_management
-from modules.homework import (
-    homework_management,
-    archived_homework
-)
+from modules.homework import homework_management, archived_homework
 
 from utils.datetime_utils import today_str
 

85ebc2722e858558264da9815e13f53eb39dc75b
karlen-tsatinyan <karlen.tsatinyan.ds@gmail.com>
2026-09-08 17:43:05 -0700

    Remove cookie test output from app.py
    Removed test code for cookie manager.

--- app.py
+++ app.py
@@ -55,8 +55,6 @@ REMEMBER_ME_SECONDS = (
 cookie_manager = stx.CookieManager(
     key="advanced_math_cookie_manager"
 )
-# delete later
-st.write("COOKIE TEST:", cookie_manager.get("advanced_math_remember_me"))
 
 
 # ==========================================================
